5ec91b96293f4374995f76d92a1560ef94bb09d6
Xavier Gourmandin <xavier.gourmandin@capgemini.com>
2019-03-25 09:42:40 +0100

    add result table

---
+++ src/components/race-result-table.unit.js
@@ -0,0 +1,7 @@
+import RaceResultTable from './race-result-table'
+
+describe('@components/race-result-table', () => {
+  it('exports a valid component', () => {
+    expect(RaceResultTable).toBeAComponent()
+  })
+})
