dc6e3e9a3db41a80c50ae7bafb9a6e2bdc50cbfa
Xavier Gourmandin <xavier.gourmandin@capgemini.com>
2019-05-17 11:09:31 +0200

    Reorganize project to handle backend-frontend development

---
+++ server/src/main/java/org/gx/chronorun/ChronorunApplication.java
@@ -0,0 +1,13 @@
+package org.gx.chronorun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronorunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChronorunApplication.class, args);
+    }
+
+}
